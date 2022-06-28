ab06415e24933f317708d0903b2136e2feda35b5
ChetanGavanali <chetangavanali@gmail.com>
2022-06-28 20:23:06 +0530

    Added MaxInteger

---
+++ src/com/bridgelabz/MaxInteger.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+public class MaxInteger {
+
+    public static void findMaximum(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
+        Integer maximumNumber;
+
+        if (firstNumber.compareTo(secondNumber) > 0 && firstNumber.compareTo(thirdNumber) > 0) {
+            maximumNumber = firstNumber;
+        } else if (secondNumber.compareTo(firstNumber) > 0 && secondNumber.compareTo(thirdNumber) > 0) {
+            maximumNumber = secondNumber;
+        } else {
+            maximumNumber = thirdNumber;
+        }
+
+        System.out.println("The maximum number is: " + maximumNumber);
+    }
+
+
+    public static void main(String[] args) {
+
+        Integer firstNumber = 1, secondNumber = 2, thirdNumber = 3;
+        findMaximum(firstNumber, secondNumber, thirdNumber);
+
+    }
+}
